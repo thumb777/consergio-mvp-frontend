e49291777e154247a414bd75a8d67e8bdaccd4dc
thumb777 <132043425+thumb777@users.noreply.github.com>
2025-04-22 03:32:59 -0400

    Update AuthenticateCallback.jsx

--- src/pages/AuthenticateCallback.jsx
+++ src/pages/AuthenticateCallback.jsx
@@ -1,31 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuth, AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import { useNavigate } from "react-router-dom";
 import Loading from "../components/Loading";
-import { useAuthContext } from "../contexts/AuthContext";
 
 const AuthenticateCallback = () => {
-  const { user, isLoaded } = useAuth();
-  const { loading } = useAuthContext();
-  const navigate = useNavigate();
-  const [isRedirecting, setIsRedirecting] = useState(true);
-
-  useEffect(() => {
-    if (isLoaded && user) {
-      if (user.createdAt === user.updatedAt) {
-        navigate("/onboarding", { replace: true });
-      } else {
-        navigate("/", { replace: true });
-      }
-      setIsRedirecting(false);
-    }
-  }, [user, isLoaded, navigate]);
-
   return (
-    <>
-      {(loading || isRedirecting) && <Loading isLoading={true} />}
-      <AuthenticateWithRedirectCallback />
-    </>
+    <div className="min-h-screen flex items-center justify-center">
+      <Loading isLoading={true} />
+      <AuthenticateWithRedirectCallback 
+        afterSignInUrl="/"
+        afterSignUpUrl="/onboarding"
+      />
+    </div>
   );
 };
 
