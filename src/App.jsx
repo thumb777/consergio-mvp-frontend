4e3f800db02a86e396de412d9e3660e7eedcc499
thumb777 <thomaswilliams.service2@gmail.com>
2025-04-22 11:33:49 -0400

    app.jsx change;

--- src/App.jsx
+++ src/App.jsx
@@ -110,12 +110,12 @@ function App() {
               ref={logoutMenuRef}
               className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10"
             >
-              <a 
+              {/* <a 
                 href="/profile" 
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
               >
                 View Profile
-              </a>
+              </a> */}
               <button
                 onClick={handleSignOut}
                 className="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
